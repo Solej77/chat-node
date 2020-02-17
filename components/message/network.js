8a7fcb51439a67a87c737f4f7e6c5486862d61ac
Solej77 <ing.josgalo@gmail.com>
2020-02-17 13:04:04 -0600

    Storing data in database

--- components/message/network.js
+++ components/message/network.js
@@ -4,12 +4,13 @@ const controller = require('./controller');
 const response = require('../../network/response');
 
 router.get('/', function(req, res) {
-  console.log(req.headers);
-  res.header({
-    "custom-header": "Header Personalizado",
-    "prueba": "prueba"
-  });
-  response.success(req, res, 'Lista de mensajes');
+  controller.getMessages()
+    .then(messageList => {
+      response.success(req, res, messageList, 200);
+    })
+    .catch(e => {
+      response.error(req, res, 'Unexpected Error', 500, e);
+    })
 });
 
 router.post('/', function(req, res) {
@@ -21,18 +22,6 @@ router.post('/', function(req, res) {
     .catch(e => {
       response.error(req, res, 'Información invalida', 400, 'Error en el controlador');
     });
-
-  if(req.query.error == "ok"){
-    
-    return false;
-  }
-  
-});
-
-router.delete('/', function(req, res) {
-  console.log(req.query);
-  console.log(req.body)
-  res.status(201).send({error: '', body:'Creado correctramente'});
 });
 
 module.exports = router;
