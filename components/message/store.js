a76fd93b4196588d58fa42a3534cbefb72b1b048
Solej77 <ing.josgalo@gmail.com>
2020-02-21 08:15:07 -0600

    Manage connections to the database from the API

--- components/message/store.js
+++ components/message/store.js
@@ -1,17 +1,4 @@
-const db = require('mongoose');
 const Model = require('./model');
-
-const USER = 'db_user_trades';
-const PASSWORD = 'Wpk63rUUzEo6';
-const DB_NAME = 'trades_db';
-const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@cluster0-zinxd.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
-
-db.Promise = global.Promise;
-db.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-console.log('[db] Conectada con éxito');
 
 function addMessage(message) {
   // list.push(message);
--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const db = require('./db');
+
+const USER = 'db_user_trades';
+const PASSWORD = 'Wpk63rUUzEo6';
+const DB_NAME = 'trades_db';
+const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@cluster0-zinxd.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+
+db.connect(MONGO_URI);
+
+
 const router = require('./network/routes');
 
 const app = express();
