8deead892a07a1db341859e09d2fae2ed86c40b5
Solej77 <ing.josgalo@gmail.com>
2020-02-22 18:08:47 -0600

    Connect the API to the WebSocket server

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const app = express();
+const server = require('http').Server(app);
 
+const bodyParser = require('body-parser');
+const socket = require('./socket');
 const db = require('./db');
+const router = require('./network/routes');
 
 const USER = 'db_user_trades';
 const PASSWORD = 'Wpk63rUUzEo6';
 const DB_NAME = 'trades_db';
 const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@cluster0-zinxd.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
-
 db.connect(MONGO_URI);
 
-
-const router = require('./network/routes');
-
-const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+socket.connect(server);
 
 router(app);
 
 app.use('/app', express.static('public'));
 
-app.listen(3000);
+server.listen(3000);
 console.log('Server in http://localhost:3000/');
